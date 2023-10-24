54fb7916e848654d6dc951daa7c01896b116f104
Xevhra <garciarodriguezalvaro12@gmail.com>
2023-10-24 12:27:37 +0200

    soy una avioneta

---
+++ Cliente/Tema 2/01.js
@@ -0,0 +1,14 @@
+/* Crear un programa que solicite al usuario nombre y apellidos y devuelva:
+    El tamaño del nombre más los apellidos sin contar los espacios.
+    La cadena completa en minúsculas y mayúsculas.
+    La división del nombre, apellido1 y apellido2 en tres líneas distintas.
+    Una propuesta de nombre usuario de la siguiente forma: inicial del nombre, primeras 
+tres letras del apellido1 y primeras tres letras del apellido2. Ejemplo: Juan Martín López  → jmarlop */
+
+let nombre = prompt('Elija su nombre');
+let apellido1 = prompt('Primer apellido');
+let apellido2 = prompt('Segundo apellido');
+
+function tamañoNombre {
+    
+}
